f312f1aafd0bbfde9abc7f7f192d37fb39207605
Cristian M <camunoz2@gmail.com>
2024-12-29 15:00:55 -0300

    fix: simplify code

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,9 @@
 import express from "express";
-import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
-app.use(express.json());
+const PORT = process.env.EXPRESS_PORT || 3000;
 
-app.use("/usuarios", userRoutes);
-
-app.use((req, res) => {
-  res.status(400).json({ error: "Page not found" });
+app.listen(PORT, () => {
+  console.log(`Servidor en ${PORT}`);
 });
-
-export default app;
